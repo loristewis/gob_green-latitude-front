c38c016197c00b875dd336f0f63eb6ac6889c488
Loris Birkemeyer <loris.birkemeyer@edu.gobelins.fr>
2022-03-04 16:45:54 +0100

    feat: test api request with axios

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>activities</h1>
+  <pre>
+    {{ activities }}
+  </pre>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ApiTest',
+  data () {
+    return {
+      activities: [],
+      error: null
+    }
+  },
+  async mounted () {
+    try {
+      const response = await axios.get('/activities', {
+        baseURL: 'http://localhost:1337/api'
+      })
+      console.log(response)
+
+      this.activities = response.data.data
+    } catch (error) {
+      this.error = error;
+    }
+  }
+}
+</script>
