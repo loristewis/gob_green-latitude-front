cb62e6d1b9be47023cd253f53ac17300bb013863
Elsa Delmas <elsad@datagif.fr>
2022-05-16 15:41:49 +0200

    styles bouton radio

--- src/components/lib/RadioButton.vue
+++ src/components/lib/RadioButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="radio-button"></div>
+  <div class="radio-button" :class="selected ? 'selected' : ''"></div>
 </template>
 
 <script>
@@ -14,4 +14,24 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.radio-button {
+  height: 18px;
+  width: 18px;
+  border-radius: 18px;
+  border: 2px solid var(--color-blue-gray);
+
+  &.selected {
+    &::before {
+      content: '';
+      display: block;
+      height: 10px;
+      width: 10px;
+      border-radius: 10px;
+      background-color: var(--color-blue-gray);
+      top: 2px;
+      left: 2px;
+    }
+  }
+}
+</style>
