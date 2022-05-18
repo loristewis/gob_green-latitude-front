ff028a4167c1e667d0e6946ee38dbbebfb30fb13
Elsa Delmas <elsad@datagif.fr>
2022-05-18 19:35:41 +0200

    wip écran de fin

--- src/components/Game/PostcardContainer.vue
+++ src/components/Game/PostcardContainer.vue
@@ -1,7 +1,23 @@
-<template>Postcard!</template>
+<template>
+  <div>
+    Postcard!
+    <Button @click="newGame">Nouvelle partie ?</Button>
+  </div>
+</template>
 
 <script>
+import { useStore } from '@/store'
+
+import { Button } from '@/components/lib'
+
 export default {
   name: 'PostcardContainer',
+  components: { Button },
+  setup() {
+    const store = useStore()
+    return {
+      store,
+    }
+  },
 }
 </script>
