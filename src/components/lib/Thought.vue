a1bb95ec79a6fde60d26084eb8130f3dd4b33ba0
Elsa <elsa.delmas@protonmail.com>
2022-06-08 16:45:36 +0200

    fix pensées

--- src/components/lib/Thought.vue
+++ src/components/lib/Thought.vue
@@ -37,27 +37,19 @@ export default {
   left: 50%;
   top: 20px;
   transform: translateX(-50%);
-  width: 100%;
   pointer-events: none;
-  transition: 0.2s ease-in-out;
+  width: 90%;
+  margin: 0 auto;
+  padding: 12px 16px;
+  opacity: 0.9;
+  transition: opacity 0.2s ease-in-out, filter 0.2s ease-in-out,
+    top 0.4s ease-in-out;
 
-  .thought {
-    top: 20px;
-    width: max-content;
-    max-width: 90%;
-    margin: 0 auto;
-    padding: 12px 16px;
-    opacity: 0.9;
-    top: 0;
-    transition: opacity 0.4s ease-in-out, filter 0.3s ease-in-out,
-      top 0.8s ease-in-out;
-
-    &.hidden {
-      opacity: 0;
-      pointer-events: none;
-      top: -20px;
-      filter: blur(5px);
-    }
+  &.hidden {
+    opacity: 0;
+    pointer-events: none;
+    top: -20px;
+    filter: blur(5px);
   }
 }
 </style>
