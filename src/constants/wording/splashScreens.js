1da5ed885180d66e99f42752089a6dde2b191dc1
Elsa <elsa.delmas@protonmail.com>
2022-06-09 13:39:45 +0200

    màj splash screens

--- src/constants/wording/splashScreens.js
+++ src/constants/wording/splashScreens.js
@@ -5,18 +5,22 @@ import TransportationIcon from '@/assets/splash/transportation.png'
 
 export const splashScreens = {
   destination: {
+    type: 'destination',
     tagline: 'Choisis ta destination !',
     image: DestinationIcon,
   },
   transportation: {
+    type: 'transportation',
     tagline: 'Achète ton billet !',
     image: TransportationIcon,
   },
   accommodation: {
+    type: 'accommodation',
     tagline: 'Réserve ton logement !',
     image: AccommodationIcon,
   },
   activities: {
+    type: 'activities',
     tagline: 'Organise tes activités !',
     image: ActivitiesIcon,
   },
