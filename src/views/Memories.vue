fb24566b7abdc4b841db3bc96d9e1321e8ce3383
Elsa <elsa.delmas@protonmail.com>
2022-06-09 13:40:24 +0200

    wip page souvenirs

--- src/views/Memories.vue
+++ src/views/Memories.vue
@@ -1,21 +1,42 @@
 <template>
-  <Container>
+  <Container class="memories-container">
     <Menu />
-    <p>Souvenirs</p>
-    <p>~WIP~</p>
+    <BaseCard class="memories-card">
+      <Sticker theme="rose-yellow" class="memories-title" big
+        >Souvenirs</Sticker
+      >
+      <p class="memories-intro">
+        Section en construction... Revenez plus tard !
+      </p>
+    </BaseCard>
   </Container>
 </template>
 
 <script>
-import { Container, Menu } from '@/components/lib'
+import { Container, Menu, BaseCard, Sticker } from '@/components/lib'
 
 export default {
   name: 'Memories',
   components: {
     Container,
+    BaseCard,
+    Sticker,
     Menu,
   },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.memories-container {
+  .memories-card {
+    margin: 0 32px;
+  }
+
+  .memories-title {
+    position: absolute;
+    top: -16px;
+    left: -16px;
+    transform: rotate(-3deg);
+  }
+}
+</style>
