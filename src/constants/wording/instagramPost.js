68431929c9e5ec8bca02d8fc6a2f26ee935cc680
Elsa <elsa.delmas@protonmail.com>
2022-06-10 17:04:33 +0200

    wording

--- src/constants/wording/instagramPost.js
+++ src/constants/wording/instagramPost.js
@@ -6,31 +6,67 @@ export const instagramPost = {
         loin: [
           'au rythme du réchauffement climatique on aura peut-être le même climat d’ici quelques années 😉',
         ],
-        bof: ['texte bof'],
-        cher: ['texte cher'],
+        bof: [
+          'super découverte de cette région ! des fois pas besoin de partir loin pour profiter de ses vacances ! 🤩',
+        ],
+        cher: [
+          'des vacances comme je les aime, cocktails et soirées en bord de plage, la luxury life me réussit bien ! vivement les prochaines ! 🥳',
+        ],
       },
     },
     hashtags: {
-      global: ['victoire'],
-      loin: ['loin'],
-      bof: ['bof'],
-      cher: ['cher'],
+      global: [
+        'summer',
+        'relax',
+        'fulldetente',
+        'travel',
+        'muchfun',
+        'besttrip',
+        'wahou',
+        'sunkissed',
+        'mercureretrograde',
+      ],
+      loin: ['decouverte', 'faraway', 'tourdumonde', 'depaysement'],
+      bof: ['chilling', 'break', 'nosregionsontdutalent', 'voyagerlocal'],
+      cher: [
+        'viedereve',
+        'cocktailz',
+        'fancy',
+        'luxurylifestyle',
+        'chic',
+        'millionaireliving',
+      ],
     },
   },
   defeat: {
     caption: {
       intro: ['enfin de retour de ::destination::...'],
       text: {
-        pollution: ['texte pollution'],
-        wellness: ['texte burnout'],
-        budget: ['texte broke'],
+        pollution: [
+          'l’envers du décor est bien différent de ce que l’on peut imaginer… je m’attendais à des bancs de poissons, pas des bancs de touristes… 😬',
+        ],
+        wellness: [
+          'un souvenir de ces vacances qui on coupé court… on part pour se reposer et on revient encore plus crevé… retour au bureau dès lundi 😫',
+        ],
+        budget: [
+          'personne ne m’avait prévenu du coût exorbitant de la vie là bas… pas le choix, on va taper dans les fonds de placards ce mois-ci 😞',
+        ],
       },
     },
     hashtags: {
-      global: ['défaite'],
-      pollution: ['apocalypse'],
-      wellness: ['burnout'],
-      budget: ['broke'],
+      global: [
+        'coupdesoleil',
+        'aieaieaie',
+        'jpp',
+        'auboutdurolls',
+        'pls',
+        'aouch',
+        'misere',
+        'mercureretrograde',
+      ],
+      pollution: ['climat', 'consommerautrement?', 'greenwashing'],
+      wellness: ['burnout', 'stress', 'deprime'],
+      budget: ['broke', 'plusunrond'],
     },
   },
 }
