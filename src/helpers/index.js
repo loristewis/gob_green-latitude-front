d0080a970f48e48b4845ac62588f0c812d0275f4
Elsa <elsa.delmas@protonmail.com>
2022-06-11 11:23:22 +0200

    fix (retour en arrière)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -21,13 +21,13 @@ export const shuffleArray = (array) => {
 export const cleanString = (string) => {
   let result = string
 
-  result = string.replaceAll('de le', 'du')
-  result = result.replaceAll('de Le', 'du')
+  result = string.replace('de le', 'du')
+  result = result.replace('de Le', 'du')
 
-  result = result.replaceAll('à le', 'au')
-  result = result.replaceAll('à Le', 'au')
+  result = result.replace('à le', 'au')
+  result = result.replace('à Le', 'au')
 
-  result = result.replaceAll(/de ([a, e, i, o, u, y])/, "d'$1")
+  result = result.replace(/de ([a, e, i, o, u, y])/, "d'$1")
 
   return result
 }
